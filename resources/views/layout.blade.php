87550564468dc2dba66782bf411a2208c72c097e
Mulia Arifandi Nasution <mul14.net@gmail.com>
2015-10-20 16:13:13 +0700

    Add default layout

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>ACL Demo - @yield('title')</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        body {
+            padding-top: 60px;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    @yield('content')
+</div>
+</body>
+</html>
