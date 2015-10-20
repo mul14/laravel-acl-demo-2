c816885b46da325444cf0971df0b85b55670eede
Mulia Arifandi Nasution <mul14.net@gmail.com>
2015-10-20 15:59:30 +0700

    Add specific User-Role methods

--- app/User.php
+++ app/User.php
@@ -46,4 +46,44 @@ class User extends Model implements AuthenticatableContract,
     {
         return $this->hasManyThrough(Permission::class, Role::class);
     }
+
+    public function isSuper()
+    {
+       if ($this->roles->contains('name', 'super')) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function hasRole($role)
+    {
+        if ($this->isSuper()) {
+            return true;
+        }
+
+        if (is_string($role)) {
+            return $this->role->contains('name', $role);
+        }
+
+        return !! $this->roles->intersect($role)->count();
+    }
+
+    public function assignRole($role)
+    {
+        if (is_string($role)) {
+            $role = Role::where('name', $role)->first();
+        }
+
+        return $this->roles()->attach($role);
+    }
+
+    public function revokeRole($role)
+    {
+        if (is_string($role)) {
+            $role = Role::where('name', $role)->first();
+        }
+
+        return $this->roles()->detach($role);
+    }
 }
